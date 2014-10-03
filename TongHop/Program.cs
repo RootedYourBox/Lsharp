dad5dd14735b788bf869d4fedfc05f18aedf2e6e
ndd305 <ndd3591@gmail.com>
2014-10-04 02:50:17 +0700

    Added TongHop

---
+++ TongHop/Program.cs
@@ -0,0 +1,20 @@
+﻿using LeagueSharp.Common;
+
+namespace TongHop
+{
+	class Program
+	{
+
+		// ReSharper disable once UnusedParameter.Local
+		private static void Main(string[] args)
+		{
+			CustomEvents.Game.OnGameLoad += Game_Start;
+		}
+
+		private static void Game_Start(System.EventArgs args)
+		{
+			// ReSharper disable once ObjectCreationAsStatement
+			new LS();
+		}
+	}
+}
