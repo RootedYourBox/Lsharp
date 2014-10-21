3664cfd120d553f5db3264c4a8d580836c6ce69c
ndd305 <ndd3591@gmail.com>
2014-10-21 22:12:17 +0700

    new BaseUlt

---
+++ BaseUlt/BaseUlt/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using LeagueSharp;
+using LeagueSharp.Common;
+
+namespace BaseUlt2
+{
+    class Program
+    {
+        public static Helper Helper;
+        private static void Main(string[] args)
+        {
+            CustomEvents.Game.OnGameLoad += Game_OnGameLoad;
+        }
+
+        static void Game_OnGameLoad(EventArgs args)
+        {
+            Helper = new Helper();
+            var baseUlt = new BaseUlt();
+        }
+    }
+}
